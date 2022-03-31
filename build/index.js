de2ec8b663c8075c357bcc35c52d96dc06e61f23
니꼬 <nico@nomadcoders.co>
2022-03-31 16:34:57 +0900

    4.1 Targets

---
+++ build/index.js
@@ -0,0 +1,8 @@
+class Block {
+    constructor(data) {
+        this.data = data;
+    }
+    static hello() {
+        return "hi";
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+class Block {
+  constructor(private data: string) {}
+  static hello() {
+    return "hi";
+  }
+}
