0e223608f964bca86beef164dda22b691744bef2
니꼬 <nico@nomadcoders.co>
2022-04-01 18:11:11 +0900

    4.7 Chain

--- build/index.js
+++ build/index.js
@@ -1,2 +1,41 @@
 "use strict";
-console.log("hi");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = __importDefault(require("crypto"));
+class Block {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
+        this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto_1.default.createHash("sha256").update(toHash).digest("hex");
+    }
+}
+class Blockchain {
+    constructor() {
+        this.blocks = [];
+    }
+    getPrevHash() {
+        if (this.blocks.length === 0)
+            return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    addBlock(data) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    getBlocks() {
+        return [...this.blocks];
+    }
+}
+const blockchain = new Blockchain();
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+console.log(blockchain.getBlocks());
--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,33 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+console.log(blockchain.getBlocks());
