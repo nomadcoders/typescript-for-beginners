f17fb7c779c255e5bd42b8999b02d6c08072a362
니꼬 <nico@nomadcoders.co>
2022-04-01 17:54:59 +0900

    4.6 DefinitelyTyped

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,6 @@ class Block implements BlockShape {
   }
   static calculateHash(prevHash: string, height: number, data: string) {
     const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
